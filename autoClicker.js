985d4a464868494613033a6e01612782fd2236b8
kevin <kevintage83@users.noreply.github.com>
2019-09-16 22:53:01 -0400

    Create autoClicker.js
    The following contains several test scripts for an applet that clicks a button on a given web page. Can be executed in the browser's console.

---
+++ autoClicker.js
@@ -0,0 +1,13 @@
+/*var i = 0;
+while (i < 1) {
+setInterval(function(){document.getElementById('plus1').click();}, 2000);
+i++;
+}
+
+for (i = 0; i < 1; i++) {
+setInterval(function(){document.getElementById('plus1').click();}, 2000);
+}
+
+setInterval(function(){document.getElementById('plus1').click();}, 2000);*/
+
+setInterval(()=>document.getElementById('plus1').click(),1000);
